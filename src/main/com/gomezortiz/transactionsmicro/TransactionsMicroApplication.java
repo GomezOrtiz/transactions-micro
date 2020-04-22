7948663f892612a344d9e3f6205bbe62959c2516
GomezOrtiz <david.gomez.mail@gmail.com>
2020-04-22 12:41:31 +0200

    Initial setup

---
+++ src/main/com/gomezortiz/transactionsmicro/TransactionsMicroApplication.java
@@ -0,0 +1,13 @@
+package com.gomezortiz.transactionsmicro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsMicroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionsMicroApplication.class, args);
+	}
+
+}
---
+++ src/test/com/gomezortiz/transactionsmicro/TransactionsMicroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gomezortiz.transactionsmicro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsMicroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
